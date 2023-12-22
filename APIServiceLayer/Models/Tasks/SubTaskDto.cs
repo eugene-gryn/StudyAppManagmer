99b93b0b5556f2e6adbf55fee543aceb4239bb0b
Yevhenii Hryn <6867668@stud.nau.edu.ua>
2023-12-23 01:45:54 +0200

    Added new entity of dtos. Filled fiction list of tasks and created new task component.

---
+++ APIServiceLayer/Models/Tasks/SubTaskDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIServiceLayer.Models.Tasks
+{
+    public class SubTaskDto
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+
+    }
+}
---
+++ APIServiceLayer/Models/Tasks/TaskDto.cs
@@ -0,0 +1,13 @@
+﻿namespace APIServiceLayer.Models.Tasks;
+
+public class TaskDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public int Priority { get; set; }
+    public int Status { get; set; }
+
+    public List<SubTaskDto> SubTasks { get; set; }
+    
+}
