d082242ba8b996e68ad19ecad427596f028213b9
Eugene Hryn <fgimminner@gmail.com>
2024-01-08 13:26:37 +0200

    Designed popover to change due date of task

--- StudyAppManagmer/Pages/Components/TaskCard.razor.cs
+++ StudyAppManagmer/Pages/Components/TaskCard.razor.cs
@@ -1,8 +1,16 @@
-﻿namespace StudyAppManagement.Pages.Components;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StudyAppManagement.Pages.Components;
 
 public partial class TaskCard
 {
-
+    private Task RescheduleTaskDue()
+    {
+        Console.WriteLine($"Reschedule task {_tempTaskChangeDueDate?.ToShortDateString()}");
+        if (_tempTaskChangeDueDate != null) Task.Deadline = _tempTaskChangeDueDate.Value;
+        SetRescheduleTaskPopoverVisibility(false);
+        return System.Threading.Tasks.Task.CompletedTask;
+    }
 }
 
 public enum ShortView
